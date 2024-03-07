62dd9bb7cfbf6975ded49aff1d4913fb3657767d
Dornavan <130479724+dorndickence@users.noreply.github.com>
2024-03-07 17:30:37 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -730,7 +730,7 @@ import {
    }
    
    function ucapan() {
-    const time = moment.tz("Asia/Kolkata").format("HH")
+    const time = moment.tz("Africa/Nairobi").format("HH")
     let res = "Good morning ☀️"
     if (time >= 4) {
      res = "Good Morning 🌄"
